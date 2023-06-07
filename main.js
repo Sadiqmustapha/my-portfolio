6ca934ef53f6f01d82303913ea602796e30858dd
Sadiqmustapha <115342247+Sadiqmustapha@users.noreply.github.com>
2023-06-06 22:57:00 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+// nav calls
+
+let menuIcon = document.getElementById("menu-icon");
+let nav = document.querySelector("nav");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("fa-times");
+    nav.classList.toggle("active");
+}
+
+if(menuIcon){
+    menuIcon.classList.add('active');
+}
+
+/**
+*!scroll activity
+**/
+
+window.onscroll = () =>{
+    nav.classList.remove('active')
+    menuIcon.classList.remove('fa-times')
+}
+
+
+document.addEventListener('DOMcntLoader', () =>{
+    const expandMore = document.querySelectorAll('[expand-more]')
+
+    function expand(){
+        const showContent = document.getElementById(this.dataset.target)
+        if (showContent.classList.contains('expand-active')){
+            this.innerHTML = this.dataset.showtext;
+        } else{
+            this.innerHTML = this.dataset.hidetext
+        }
+        showContent.classList.toggle('expand-active')
+    }
+
+    expandMore.forEach(expandMore =>{
+        expandMore.addEventListener('click', expand)
+    })
+})
+
+/**
+ * ? these are the read more js codes
+ * **/
+
+
+function myFunction() {
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
